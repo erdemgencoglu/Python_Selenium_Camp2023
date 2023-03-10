a6e42294a23460b671c78653f1ca95f4d6259279
Erdem <gencogluerdem@gmail.com>
2023-03-10 14:23:04 +0300

    refactor

---
+++ Lesson_1/HomeWork1/Kodlama_io.py
@@ -0,0 +1,5 @@
+# Dersler liste de tutulmuş olabilir
+# Ders Başlıkları String tipindedir
+# Tamamlama oranları % hariç sayı tipinde ve % karakteri string olabilir
+# Tamamlama oranları gerçekleşince bu koşula bağlı farklı olaylar tetiklenmekte olabilir (If/Else)
+# Ders listesi döngüler ile listelenmiş olabilir.
